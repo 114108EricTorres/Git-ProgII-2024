cfa78ea30267a19455d5202136cd4f125446a6e4
114108EricTorres <114108@tecnicatura.frc.utn.edu.ar>
2024-08-20 21:13:49 -0300

    Ejercicio 1.1 | Clase 20/08

---
+++ Pila.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase20_08
+{
+    public class Pila : ICollection
+
+    {  
+        public int contador { get; set; }
+        object[] elementos = new Object[0];
+        int tamaño = 0;
+     
+        public Pila(int tamaño)
+        {
+            elementos = new object[tamaño];
+            contador = 0;
+        }
+
+        public bool añadir(object objeto)
+        {
+
+            if (objeto[] = null)
+            {
+                return false;
+            }
+            else
+            {
+                elementos[contador] = objeto;
+                contador++;
+                return true;
+            }
+          
+        }
+
+        public bool estaVacia()
+        {
+          if (elementos.Length == 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public object extraer()
+        {
+            contador = 0;
+            Object primerElemento = elementos [contador];
+            return primerElemento;
+            primerElemento = null;
+            contador ++; 
+        }
+
+        public object primero()
+        {
+            contador = 0;
+            Object primerElemento = elementos[contador];
+            return primerElemento;
+            
+        }
+    }
+}
